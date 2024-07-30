3d2d68c2675803c78dab6341b90e36b11f03fc84
sanjanagupta21 <sanjanaguptadps@gmail.com>
2024-07-30 15:03:33 +0530

    updated code and added requirements.txt

--- app/views.py
+++ app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def reservation(request):
+    return render(request,'reservation.html')
+
 # Create your views here.
