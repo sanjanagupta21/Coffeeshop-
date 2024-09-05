c011a9105cd62f6c8d7a9effac7f018ac3e50df0
nhk1720 <nikhilkumar047075@gmail.com>
2024-09-05 13:04:21 +0530

    code

--- app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-def home(request):
-    return render(request, 'home.html')
-
-def reservation(request):
-    return render(request,'reservation.html')
-
-# Create your views here.
