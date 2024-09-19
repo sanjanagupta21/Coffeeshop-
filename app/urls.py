4fecb563c3d7b3cbe2af2bc995d47cace751afef
sanjanagupta21 <sanjanaguptadps@gmail.com>
2024-09-19 16:06:22 +0530

    updated working code

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from .views import home, success, book_table,ReservationListCreate
 
 urlpatterns = [
     path('', home, name='home'),
+    
     # path('reservation/', views.reservation_view, name='reservation'),
     # path('reservation/', reservation_view, name='reservation'),
     path('reservation/', views.book_table, name='book_table'),
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'home.html')
 
+
 # def reservation(request):
 #     return render(request,'reservation.html')
 
